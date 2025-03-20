48dc8cb7eec3d5d413e396d632dd1738a7265509
Dominique Garmier <dominique@garmier.ch>
2025-03-20 07:52:57 +0100

    feat: changed typst version

--- lua/base/plugins.lua
+++ lua/base/plugins.lua
@@ -108,7 +108,6 @@ require("lazy").setup({
 	{
 		"chomosuke/typst-preview.nvim",
 		lazy = false, -- or ft = 'typst'
-		version = "1.*",
 		opts = {}, -- lazy.nvim will implicitly calls `setup {}`
 	},
 })
