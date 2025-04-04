e49c5ceb3d2a2f2ae3fe20738c0a505776a71f09
Dominique Garmier <dominique@garmier.ch>
2025-04-04 11:09:06 +0200

    fix: for remap accep copilot

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -2,22 +2,23 @@ local copilot = require("copilot")
 local suggestion = require("copilot.suggestion")
 
 copilot.setup({
-  suggestion = {
-    auto_trigger = true,
-  },
-  filetypes = {
-    markdown = true
-  },
-  auto_start = true,
+	suggestion = {
+		auto_trigger = true,
+	},
+	filetypes = {
+		markdown = true,
+	},
+	auto_start = true,
 })
 
 local accept = function()
-  if suggestion.is_visible() then
-    suggestion.accept()
-  else
-    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-l>", true, true, true), "n")
-  end
+	if suggestion.is_visible() then
+		suggestion.accept()
+	else
+		vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-l>", true, true, true), "n")
+	end
 end
 
-vim.keymap.set("i", "<C-l>", function() accept() end)
-
+vim.keymap.set("i", "<C-l>", function()
+	accept()
+end, { noremap = true, silent = true })
